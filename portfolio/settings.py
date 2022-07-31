b0befc691982c47d9a0c11cd9a2c2e617c7602c0
Cell <suhail9453531504@gmail.com>
2022-07-31 14:01:39 +0530

    Add env vars

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -28,7 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = config('DEBUG', cast=bool)
-DEBUG = True
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
