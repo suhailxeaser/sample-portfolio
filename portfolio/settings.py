accaa76b52455ce1a1457a890e9fa8f708dd334b
Cell <suhail9453531504@gmail.com>
2022-07-31 11:19:58 +0530

    Changed DEBUG value

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+# DEBUG = config('DEBUG', cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
